d9762ed51a0661e72b3dbda6174bca281a5e9c5d
lowlighter <simon.lecoq@live.fr>
2020-04-05 23:12:38 +0200

    Create draft

---
+++ maps/overworld/tileset.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="overworld" tilewidth="16" tileheight="16" tilecount="32768" columns="256">
+ <editorsettings>
+  <export target="tileset.json" format="json"/>
+ </editorsettings>
+ <image source="tileset.textures.png" trans="ffff99" width="4096" height="2048"/>
+</tileset>
