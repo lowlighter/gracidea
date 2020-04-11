87424037ffc68829f809b672b6cdbb973b04e6d0
lowlighter <simon.lecoq@live.fr>
2020-04-11 17:23:31 +0200

    Update app.js

--- server/app.js
+++ server/app.js
@@ -8,7 +8,8 @@
 //Dev mode
   if (argv.dev) {
     console.log("Launching in dev environment".cyan)
-    app.get("/js/gracidea.js", (req, res) => res.sendFile(path.join(__dirname, "..", "client/src/gracidea.js")))
+    app.get("/js/gracidea.min.js", (req, res) => res.sendFile(path.join(__dirname, "..", "client/js/src/gracidea.js")))
+    app.use("/js", express.static("client/js/src"))
     app.get("/maps/:map/tileset.textures.png", (req, res) => res.sendFile(path.join(__dirname, "..", "/maps/", req.params.map,"/tileset.textures.raw.png")))
   }
 
