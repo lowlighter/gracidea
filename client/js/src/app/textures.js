715decf9b69445e0fa1d6b259375bd4a64b00c18
Lecoq Simon <22963968+lowlighter@users.noreply.github.com>
2020-08-24 22:37:52 +0200

    Continue road 119, add and waterfall animation (#17)

--- client/js/src/app/textures.js
+++ client/js/src/app/textures.js
@@ -1,8 +1,12 @@
-/** 
+/**
  * Animated textures.
  */
   export default {
     animated:{
+      2266:{
+        frames:[2266, 2267, 2268, 2269].map(frame => `${frame}`),
+        speed:0.025,
+      },
       2374:{
         frames:[2374, 2375, 2376, 2377, 2378, 2379, 2380, 2381].map(frame => `${frame}`),
         speed:0.075,
@@ -10,6 +14,10 @@
       429:{
         frames:[429, 430].map(frame => `${frame}`),
         speed:0.05,
+      },
+      431:{
+        frames:[431, 432].map(frame => `${frame}`),
+        speed:0.0125,
       }
     }
   }
