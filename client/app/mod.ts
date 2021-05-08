eab9521bc477a1d4688a822b9e56e031efdb3c3b
lowlighter <22963968+lowlighter@users.noreply.github.com>
2021-05-08 18:10:46 +0200

    Update mod.ts

--- client/app/mod.ts
+++ client/app/mod.ts
@@ -2,7 +2,10 @@
  * Copyright Simon Lecoq (@lowlighter)
  * @license https://github.com/lowlighter/gracidea/blob/main/README.md
  */
+
 //Imports
-import { App } from "./app.ts" //Start app
-//deno-lint-ignore no-explicit-any
-;(globalThis as any).app = new App()
+import { global } from "../../build/constants.ts"
+import { App } from "./app.ts"
+
+//Start app
+global.app = new App()
