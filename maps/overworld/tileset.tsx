dd82abb000e116e7ae119bfcf452414bff111b40
lowlighter <22963968+lowlighter@users.noreply.github.com>
2021-05-08 19:45:49 +0200

    Update

--- maps/overworld/tileset.tsx
+++ maps/overworld/tileset.tsx
@@ -1,59 +1,10 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.3" name="overworld" tilewidth="16" tileheight="16" spacing="2" margin="2" tilecount="12769" columns="113">
+<tileset version="1.5" tiledversion="1.6.0" name="overworld" tilewidth="16" tileheight="16" spacing="2" margin="2" tilecount="6328" columns="113">
  <editorsettings>
   <export target="tileset.json" format="json"/>
  </editorsettings>
- <image source="tileset.textures.raw.png" trans="000000" width="2036" height="2036"/>
- <terraintypes>
-  <terrain name="New Terrain" tile="2601"/>
-  <terrain name="New Terrain" tile="2604"/>
-  <terrain name="New Terrain" tile="3401"/>
-  <terrain name="New Terrain" tile="2616"/>
-  <terrain name="New Terrain" tile="2613"/>
- </terraintypes>
- <tile id="2487" terrain=",,,0"/>
- <tile id="2488" terrain=",,0,0"/>
- <tile id="2489" terrain=",,0,"/>
- <tile id="2490" terrain=",,,1"/>
- <tile id="2491" terrain=",,1,1"/>
- <tile id="2492" terrain=",,1,"/>
- <tile id="2499" terrain=",,,4"/>
- <tile id="2500" terrain=",,4,4"/>
- <tile id="2501" terrain=",,4,"/>
- <tile id="2502" terrain=",,,3"/>
- <tile id="2503" terrain=",,3,3"/>
- <tile id="2504" terrain=",,3,"/>
- <tile id="2600" terrain=",0,,0"/>
- <tile id="2601" terrain="0,0,0,0"/>
- <tile id="2602" terrain="0,,0,"/>
- <tile id="2603" terrain=",1,,1"/>
- <tile id="2604" terrain="1,1,1,1"/>
- <tile id="2605" terrain="1,,1,"/>
- <tile id="2612" terrain=",4,,4"/>
- <tile id="2613" terrain="4,4,4,4"/>
- <tile id="2614" terrain="4,,4,"/>
- <tile id="2615" terrain=",3,,3"/>
- <tile id="2616" terrain="3,3,3,3"/>
- <tile id="2617" terrain="3,,3,"/>
- <tile id="2713" terrain=",0,,"/>
- <tile id="2714" terrain="0,0,,"/>
- <tile id="2715" terrain="0,,,"/>
- <tile id="2716" terrain=",1,,"/>
- <tile id="2717" terrain="1,1,,"/>
- <tile id="2718" terrain="1,,,"/>
- <tile id="2725" terrain=",4,,"/>
- <tile id="2726" terrain="4,4,,"/>
- <tile id="2727" terrain="4,,,"/>
- <tile id="2728" terrain=",3,,"/>
- <tile id="2729" terrain="3,3,,"/>
- <tile id="2730" terrain="3,,,"/>
- <tile id="3287" terrain=",,,2"/>
- <tile id="3288" terrain=",,2,2"/>
- <tile id="3289" terrain=",,2,"/>
- <tile id="3400" terrain=",2,,2"/>
- <tile id="3401" terrain="2,2,2,2"/>
- <tile id="3402" terrain="2,,2,"/>
- <tile id="3513" terrain=",2,,"/>
- <tile id="3514" terrain="2,2,,"/>
- <tile id="3515" terrain="2,,,"/>
+ <image source="tileset3.png" trans="000000" width="2036" height="1010"/>
+ <wangsets>
+  <wangset name="Terrains" type="corner" tile="-1"/>
+ </wangsets>
 </tileset>
