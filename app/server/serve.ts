6b09a87a8601390aa3fdbe38db39c1a9160ec3e1
lowlighter <22963968+lowlighter@users.noreply.github.com>
2021-12-27 18:32:19 -0500

    debug: deploy static asset

--- app/server/serve.ts
+++ app/server/serve.ts
@@ -43,6 +43,7 @@ export async function serve({ port = 4000 }: { port?: number } = {}) {
       for (const assets of ["../client", "../../copyrighted"]) {
         try {
           console.debug(`trying static asset: ${new URL(`${assets}/${path}`, import.meta.url)}`)
+          console.log({...await fetch(new URL(`${assets}/${path}`, import.meta.url))})
           const body = await fetch(new URL(`${assets}/${path}`, import.meta.url)).then((response) => response.body);
           return new Response(body, { headers });
         } catch {
