2804b30ce7f34ad687e556893f7bfd91d6ad6140
lowlighter <22963968+lowlighter@users.noreply.github.com>
2022-01-03 13:11:58 -0500

    test: add generated content checks

--- app/tests/integrity_test.ts
+++ app/tests/integrity_test.ts
@@ -5,16 +5,18 @@ import * as XML from "https://deno.land/x/xml@2.0.3/mod.ts";
 import { extname } from "https://deno.land/std@0.119.0/path/mod.ts";
 
 //Ensure files are parsable
-for await (const { path, isFile } of expandGlob("{maps,copyrighted}/**/*.{tmx,tsx,tps,json}")) {
-  if (isFile) {
-    const file = path.replace(Deno.cwd(), "");
-    const ext = extname(path).substring(1);
-    Deno.test(file, async () =>
+for (const glob of ["{maps,copyrighted}/**/*.{tmx,tsx,tps,json}", "app/generated/**/*.json"]) {
+  for await (const { path, isFile } of expandGlob(glob)) {
+    if (isFile) {
+      const file = path.replace(Deno.cwd(), "");
+      const ext = extname(path).substring(1);
+      Deno.test(file, async () =>
       assert({
         json: JSON,
         tmx: XML,
         tsx: XML,
         tps: XML,
       }[ext]?.parse(await Deno.readTextFile(path))));
+    }
   }
 }
